f5c192a7c74490c4e79ba309d3a618b179e6fc64
Ppe-Donny <donny.rivera@gmail.com>
2025-07-02 16:41:20 -0400

    :white_check_mark: feat: Model and Admin Panel

--- 03-PythonDjango/cars/models.py
+++ 03-PythonDjango/cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    factory_year = models.PositiveIntegerField(blank=True, null=True)
+    model_year = models.PositiveIntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id} - {self.model}"
